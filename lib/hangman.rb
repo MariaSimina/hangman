77f799fbb82198ed8ccbc80ce5d6014e9fb8f398
Maria Simina <maria.simina_groza@yahoo.com>
2023-10-25 11:47:35 +0300

    Put hangman game in a class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,60 +1,78 @@
-puts "Hello and welcome to a new game of HANGMAN!"
+class Game
+  @@wrong_guesses = []
+  @@lives_count = 10
+  @@dashes = []
+  @@secret_word = ""
 
-def get_secret_word
-  dictionary = File.read("choices.txt")
-  dictionary_array = dictionary.split("\n")
-  secret_word = ""
-
-  until (secret_word.length > 5) && (secret_word.length < 12)
-    secret_word = dictionary_array.sample
-  end
-
-  secret_word
-end
-
-def check_guess(word_array, dashes_array, guess, wrong_guesses_array, count)
-  if word_array.include? guess
-    word_array.each_with_index do |letter, index|
-      if letter == guess
-        dashes_array[index] = guess
-      end
+  def guess
+    get_guess = ""
+    until (get_guess.length == 1) && (get_guess =~ /[[:alpha:]]/)
+      puts "Please write one letter:"
+      get_guess = gets.chomp.downcase
     end
-  else
-    puts "Wrong guess"
-    wrong_guesses_array.push(guess)
-    count[0] -= 1
+    
+    while (@@wrong_guesses.include? get_guess) || (@@dashes.include? get_guess)
+      puts "You already chose that letter. Choose another one:"
+      get_guess = gets.chomp.downcase
+    end
+
+    get_guess
+  end
+
+  def check_guess(guess)
+    secret_word = @@secret_word.split("")
+    if secret_word.include? guess
+      secret_word.each_with_index do |letter, index|
+        if letter == guess
+          @@dashes[index] = "#{guess} "
+        end
+      end
+    else
+      puts "Wrong guess"
+      @@wrong_guesses.push(guess)
+      @@lives_count -= 1
+    end
+  end
+
+  def play
+    puts "Hello and welcome to a new game of HANGMAN!"
+    puts "Try to guess the secret word"
+    self.get_secret_word
+    self.dashes
+    puts "\n#{@@dashes.join("")}"
+
+    until (@@lives_count == 0) || !(@@dashes.include? "_ ")
+      puts "You have #{@@lives_count} lives"
+
+      unless @@wrong_guesses.empty?
+        puts "Wrong guesses: #{@@wrong_guesses.join(", ")}"
+      end
+
+      get_guess = guess
+      check_guess(get_guess)
+      puts "\n#{@@dashes.join("")}"
+    end
+
+    puts "Secret word was: #{@@secret_word}"
+    puts "Game over!"
+  end
+
+  def get_secret_word
+    dictionary = File.read("choices.txt")
+    dictionary_array = dictionary.split("\n")
+    secret_word = ""
+
+    until (secret_word.length > 5) && (secret_word.length < 12)
+      secret_word = dictionary_array.sample
+    end
+
+    @@secret_word = secret_word
+  end
+
+  def dashes
+    @@secret_word.length.times{@@dashes.push "_ "}
   end
 end
 
-def clean_guess(letter, dashes_array, wrong_guesses_array)
-  while (wrong_guesses_array.include? letter) || (dashes_array.include? letter)
-    puts "You already chose that letter. Choose another one:"
-    letter = gets.chomp
-  end
-  letter
-end
-
-secret_word = get_secret_word
-
-dashes = []
-secret_word.length.times{dashes.push "_ "}
-
-wrong_guesses = []
-
-count = [10]
-
-secret_word = secret_word.split("")
-puts dashes.join(" ")
-
-until (!dashes.include? "_ ") || (count[0] == 0)
-  puts "Choose a letter:"
-  guess = gets.chomp.downcase
-  the_guess = clean_guess(guess, dashes, wrong_guesses)
-  check_guess(secret_word, dashes, the_guess, wrong_guesses, count)
-  puts "\nYou have #{count[0]} lives."
-  puts "Wrong guesses: #{wrong_guesses.join(", ")}"
-  puts dashes.join(" ")
-end
-
-puts "\nThe word was: #{secret_word.join("")}"
-puts "Game over"
+game = Game.new
+game.play
